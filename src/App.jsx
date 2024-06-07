a41f982027b5127f06b6ab3bf1a174570f9b410e
Jota2kk <j.amaral4000@gmail.com>
2024-06-07 15:32:24 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import Search from './Search'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='App'>
+        <Search placeholder="cidade"/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/test.jsx
@@ -0,0 +1,19 @@
+import "./App.css"
+
+function Teste () {
+    return (
+        <div className='searchtbm'>
+            <h1>
+                porque essa porra nao funciona?
+            </h1>
+
+            <h2>
+                olha ele
+            </h2>
+
+            <input type="text" placeholder="PORRRRRA"></input>
+        </div>
+    )
+}
+
+export default Teste;
